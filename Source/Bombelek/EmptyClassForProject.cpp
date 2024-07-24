34c44f453bb1cdf24229a3140822e5d59adde022
KacperWalasek <41973501+KacperWalasek@users.noreply.github.com>
2024-07-24 13:58:29 +0200

    First bubble material

---
+++ Source/Bombelek/EmptyClassForProject.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//#include "Bombelek.h"
+#include "EmptyClassForProject.h"
+
+EmptyClassForProject::EmptyClassForProject()
+{
+}
+
+EmptyClassForProject::~EmptyClassForProject()
+{
+}
